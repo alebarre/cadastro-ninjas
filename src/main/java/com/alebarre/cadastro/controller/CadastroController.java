513a204f81757865260fc1d70dc9bb26d8bd3c5f
Alexandre Barreto <plotartemail@gmail.com>
2025-03-19 16:49:11 -0300

    Primeiro commit

---
+++ src/main/java/com/alebarre/cadastro/controller/CadastroController.java
@@ -0,0 +1,17 @@
+package com.alebarre.cadastro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CadastroController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Boas Vindas";
+    }
+
+}
