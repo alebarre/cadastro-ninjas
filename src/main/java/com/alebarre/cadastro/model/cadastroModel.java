0de870a528a424c92d61a76cc0ad2cd25f5f2e2c
Alexandre Barreto <plotartemail@gmail.com>
2025-03-19 21:40:57 -0300

    alterando o model

--- src/main/java/com/alebarre/cadastro/model/cadastroModel.java
+++ src/main/java/com/alebarre/cadastro/model/cadastroModel.java
@@ -2,6 +2,8 @@ package com.alebarre.cadastro.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_cadastro")
 public class cadastroModel {
@@ -22,4 +24,57 @@ public class cadastroModel {
         this.idade = idade;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        cadastroModel that = (cadastroModel) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(email, that.email) && Objects.equals(idade, that.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, idade);
+    }
+
+    Override
+    public String toString() {
+        return "cadastroModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
 }
